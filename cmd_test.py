22bd5ca4d9d2e61d1b389b1149100e33de91022b
turinaf <dhalootaqubee2010@gmail.com>
2021-07-21 10:19:44 +0800

    modified cmd_test

--- cmd_test.py
+++ cmd_test.py
@@ -1,39 +1,38 @@
 import cmd
-from random import choice
-import oxo_logic, oxo_ui
+import ttt_ui, ttt_logic
 import argparse as ap
-class Oxo_cmd(cmd.Cmd):
+class Ttt_cmd(cmd.Cmd):
     intro = "Enter a command: new, resume, quit. Type 'help' or '?' for help"
-    prompt = "(oxo) "
+    prompt = "(ttt) "
     game = ""
 
 def do_new(self, arg):
-    self.game = oxo_logic.newGame()
-    oxo_ui.playGame(self.game)
+    self.game = ttt_logic.newGame()
+    ttt_ui.playGame(self.game)
    
 
 def do_resume(self, arg):
-    self.game = oxo_logic.newGame()
-    oxo_ui.playGame(self.game)
+    self.game = ttt_logic.restoreGame()
+    ttt_ui.playGame(self.game)
 
 def do_quit(self, arg):
     print("GOodbye...")
     raise SystemExit
 
 def main():
-    p = ap.ArgumentParser(description="Play a game of tic-tac-toe")
-    grp = ap.add_mutually_exclusive_group()
-    grp.add_argument("-r", "--res", "--restore", action='store_true', help="Restore old game")
-    args = p.parse_args()
-    if args.new:
-        oxo_ui.executeChoice(1)
-    elif args.res:
-        oxo_ui.executeChoice(2)
-    else:
-        while True:
-            choice = oxo_ui.getMenuChoice(oxo_ui.menu)
-            oxo_ui.executeChoice(choice)
-    # game = Oxo_cmd().cmdloop()
+    # p = ap.ArgumentParser(description="Play a game of tic-tac-toe")
+    # grp = ap.add_mutually_exclusive_group()
+    # grp.add_argument("-r", "--res", "--restore", action='store_true', help="Restore old game")
+    # args = p.parse_args()
+    # if args.new:
+    #     ttt_ui.executeChoice(1)
+    # elif args.res:
+    #     ttt_ui.executeChoice(2)
+    # else:
+    #     while True:
+    #         choice = ttt_ui.getMenuChoice(ttt_ui.menu)
+    #         ttt_ui.executeChoice(choice)
+    game = Ttt_cmd().cmdloop()
 
 if __name__=="__main__":
     main()
